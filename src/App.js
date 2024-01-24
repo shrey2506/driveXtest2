2ab8f4b6e73a52dae9266ec7687ae4ca9b19517b
shrey2506 <shreysrivastava20@gmail.com>
2024-01-24 18:52:34 +0530

    Added category filters. Todo - adding filters and updating the product lists

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import { Suspense, useEffect, useState } from 'react';
 import './App.css';
+import data from './data/data'
 
 function App() {
+  //To do adding filters for the and displaying lists on those categories. Didn't complete it due to time constraints
+   const [filteredProducts,setFiltereedProducts]=useState(data.products.data.items)
+   const [selectedCategory,setSelectedCategory]=useState('')
+   const [subCategories,setSubCategories]=useState([])
+
+
+   //extracting categories and subcategories and storing these in an object
+   let categories=data.products.data.items.reduce((acc,item)=>{
+    acc[item.category]=[...(acc[item.category ] || []) , item.subcategory]
+    return acc
+   },{})
+
+
+   const handleCategoryClick=(category)=>{
+     setSelectedCategory(category)
+     setSubCategories([...new Set(categories[category])])
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{
+        display: 'flex'
+      }}>
+        <div style={{
+          flex: 1,
+          padding: '10px',
+        }}>
+
+          <h2>Categories</h2>
+          <ul>
+          {Object.keys(categories).map((category,index)=>(
+            <li onClick={()=>handleCategoryClick(category)} key={index}>{category}</li>
+          ))}
+          </ul>
+        </div>
+        <div style={{
+          flex: 1,
+          padding: '10px',
+        }}>
+
+          <h2>Sub Categories</h2>
+
+          <ul>
+        {subCategories.map((subCategory,index)=>(
+            <li key={index}>{subCategory}</li>
+        )
+        )}
+       </ul>
+
+        </div>
+
+      </div>
+     
+  </div>
+       
+    
   );
 }
 
